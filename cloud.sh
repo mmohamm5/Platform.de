6a7392b73b124246a925d7b9d0e969b0715b6ce4
mmohamm5 <nurmohammadshawon@gmail.com>
2025-01-04 11:02:29 +0000

    Added new py & sh file for platform.de project

---
+++ cloud.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+##Author: XXXXX company
+##Editor: Tanvir
+##Topic: CLoud formation set-up for new platform.de
+
+Sudo apt-get update
---
+++ giga.py
@@ -0,0 +1,1 @@
+print ("This is for giga. new project for platform.de")
